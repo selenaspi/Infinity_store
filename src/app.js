a5cde3dd502de468de6d336d4590d888e0bb4172
Selena <ramosselena9@gmail.com>
2022-03-17 19:51:41 -0300

    Corrección

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath));
-app.listen( port, () => console.log("Servidor corriendo en el puerto " + port));
+app.listen( PORT, () => console.log("Servidor corriendo en el puerto " + PORT));
 
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
