f94c5064d2802ac59ef309220cce2915677b3b70
Selena <ramosselena9@gmail.com>
2022-03-17 19:45:40 -0300

    Correcciones

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require ('path');
-const port = 3030;
+const PORT = process.env.PORT || 3000;
 const app = express(); 
 
 const publicPath = path.resolve(__dirname, '../public');
