03660b944893e50555ff06204add0edf6b0e9087
Selena <ramosselena9@gmail.com>
2022-03-17 23:09:15 -0300

    Arreglo

--- src/app.js
+++ src/app.js
@@ -10,5 +10,5 @@ app.listen( PORT, () => console.log("Servidor corriendo en el puerto " + PORT));
 app.get("/productCart", (req, res) => {res.sendFile(path.join(__dirname, "views", "productCart.html"))});
 app.get("/login", (req, res) => {res.sendFile(path.join(__dirname, "views", "login.html"))})
 app.get("/register", (req, res) => {res.sendFile(path.join(__dirname, "views", "register.html"))})
-app.get("/productDetails", (req, res) => {res.sendFile(path.join(__dirname, "views", "productDetails.html"))})
+app.get("/productDetails", (req, res) => {res.sendFile(path.join(__dirname, "views", "ProductDetails.html"))})
 app.get("/", (req, res) => {res.sendFile(path.join(__dirname, "views", "index.html"))})
